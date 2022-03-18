b4bfab78a64d5ddd6f249ef4646473b980baf18d
Jorgep503 <jorge.p.391@gmail.com>
2022-03-18 14:36:05 -0600

    Else con *ngIf

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class PersonasComponent{
     desahabilitar = false;
-    mensaje = 'No se ha agregado ninguna persona';
+    mensaje = '';
     titulo = 'NSH';
     mostrar = false;
 
